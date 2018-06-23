7b4a127659472cef69e48e1c6e0accfd74df78c1
Ryan Govostes <rgovostes+git@gmail.com>
2018-06-23 17:02:20 +0900

    Hook up the output to a syntax highlighter

--- dist.sh
+++ dist.sh
@@ -3,8 +3,9 @@
 set -e
 
 rm -Rf dist && mkdir dist
-browserify main.js -o dist/bundle.js
+browserify -t [ babelify --presets [ env react ] ] main.js -o dist/bundle.js
 cp index.html dist/index.html
+cp node_modules/highlight.js/styles/github.css dist/theme.css
 
 if [ "$1" == "serve" ]; then
   cd dist
